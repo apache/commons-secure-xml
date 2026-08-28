07d210edf1945e3afd1fe33c29899099fe5288d2
Gary Gregory <garydgregory@gmail.com>
2026-08-28 14:50:24 -0400

    Javadoc: "hardened" -> "secure/securing" wording to match new class and method names.
    Rename test methods.

--- src/test/java/org/apache/commons/xml/SchemaLocationPropertyTest.java
+++ src/test/java/org/apache/commons/xml/SchemaLocationPropertyTest.java
@@ -38,7 +38,7 @@ import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 
 /**
- * Checks that a hardened, schema-validating parser does not fetch a schema named only through a Xerces schema-location
+ * Checks that a secure, schema-validating parser does not fetch a schema named only through a Xerces schema-location
  * property: {@code external-noNamespaceSchemaLocation} (no-namespace schema) and {@code external-schemaLocation}
  * (namespaced schema).
  *
