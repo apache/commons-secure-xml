0f2f2f22f668bd4dfb37cd12ae1c7f543f93f37a
Gary Gregory <garydgregory@gmail.com>
2026-09-14 10:06:37 -0700

    Javadoc

--- src/main/java/org/apache/commons/xml/secure/FallbackIgnoreLSResourceResolver.java
+++ src/main/java/org/apache/commons/xml/secure/FallbackIgnoreLSResourceResolver.java
@@ -59,9 +59,9 @@ final class FallbackIgnoreLSResourceResolver implements LSResourceResolver {
     }
 
     /**
-     * Gets the delegate provided by the constructor or set by {@link #setDelegate}, may be {@code null}.
+     * Gets the delegate provided by the constructor or set by {@link #setDelegate}, or {@code null}.
      *
-     * @return The delegate provided by the constructor or set by {@link #setDelegate}, may be {@code null}.
+     * @return The delegate provided by the constructor or set by {@link #setDelegate}, or {@code null}.
      */
     LSResourceResolver getDelegate() {
         return delegate;
